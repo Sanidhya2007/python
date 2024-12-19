41cd6a53ab16249e767bd78554c772349808e99d
Sanidhya2007 <166147430+Sanidhya2007@users.noreply.github.com>
2024-12-19 22:51:34 +0530

    Add files via upload

---
+++ student.py
@@ -0,0 +1,66 @@
+from tkinter import*
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+
+class Student:
+    def __init__(self,root):
+        self.root=root
+        self.root.geometry("1530x790+0+0")
+        self.root.title("face recognition system")
+
+        #first image
+        img=Image.open(r"C:\Users\Dell\Downloads\face recognition images\stu1.jpeg")
+        img=img.resize((500,130))
+        self.photoimg=ImageTk.PhotoImage(img)
+
+        f_lbl=Label(self.root,image=self.photoimg)
+        f_lbl.place(x=0,y=0,width=500,height=130)
+
+        #second image 
+        img1=Image.open(r"C:\Users\Dell\Downloads\face recognition images\stu2.jpeg")
+        img1=img1.resize((500,130))
+        self.photoimg1=ImageTk.PhotoImage(img1)
+
+        f_lbl=Label(self.root,image=self.photoimg1)
+        f_lbl.place(x=500,y=0,width=500,height=130)
+
+        #third image
+        img2=Image.open(r"C:\Users\Dell\Downloads\face recognition images\stu 3.jpeg")
+        img2=img2.resize((500,130))
+        self.photoimg2=ImageTk.PhotoImage(img2)
+
+        f_lbl=Label(self.root,image=self.photoimg2)
+        f_lbl.place(x=1000,y=0,width=550,height=130)
+
+        #bg image
+        img3=Image.open(r"C:\Users\Dell\Downloads\face recognition images\eh.jpg")
+        img3=img3.resize((1530,710))
+        self.photoimg3=ImageTk.PhotoImage(img3)
+
+        bg_img=Label(self.root,image=self.photoimg3)
+        bg_img.place(x=0,y=130,width=1530,height=710)
+
+        title_lbl=Label(bg_img,text="STUDENT MANAGEMENT SYSTEM" ,font=("times new roman",35,"bold"),bg="white",fg="darkgreen")
+        title_lbl.place(x=0,y=0,width=1530,height=45)
+        
+        main_frame=Frame(bg_img,bd=2,bg="white")
+        main_frame.place(x=20,y=50,width=1480,height=600)
+
+        # left label frame 
+        Left_frame=LabelFrame(main_frame,bd=2,bg="white",relief=RIDGE,text="Student Details",font=("times new roman",12,"bold"))
+        Left_frame.place(x=10,y=10,width=760,height=580)
+
+        # Right label frame 
+        Right_frame=LabelFrame(main_frame,bd=2,bg="white",relief=RIDGE,text="Student Details",font=("times new roman",12,"bold"))
+        Right_frame.place(x=780,y=10,width=660,height=580)
+
+
+
+
+
+if __name__ == "__main__":
+    root=Tk()
+    obj=Student(root)
+    root.mainloop()
+  
