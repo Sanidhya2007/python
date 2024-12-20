38617683a08c02c34839b121d6fe4add17668060
Sanidhya2007 <166147430+Sanidhya2007@users.noreply.github.com>
2024-12-20 21:48:49 +0530

    Update student.py 1

--- student.py
+++ student.py
@@ -51,9 +51,87 @@ class Student:
         Left_frame=LabelFrame(main_frame,bd=2,bg="white",relief=RIDGE,text="Student Details",font=("times new roman",12,"bold"))
         Left_frame.place(x=10,y=10,width=760,height=580)
 
-        # Right label frame 
-        Right_frame=LabelFrame(main_frame,bd=2,bg="white",relief=RIDGE,text="Student Details",font=("times new roman",12,"bold"))
-        Right_frame.place(x=780,y=10,width=660,height=580)
+        img_left=Image.open(r"C:\Users\Dell\Downloads\face recognition images\stu 3.jpeg")
+        img_left=img_left.resize((720,130))
+        self.photoimg_left=ImageTk.PhotoImage(img_left)
+
+        f_lbl=Label(Left_frame,image=self.photoimg_left)
+        f_lbl.place(x=5,y=0,width=720,height=130)
+
+        
+        # current course
+        current_course_frame=LabelFrame(Left_frame,bd=2,bg="white",relief=RIDGE,text="Current course information",font=("times new roman",12,"bold"))
+        current_course_frame.place(x=5,y=135,width=720,height=125)
+
+        #Department
+        dep_label=Label(current_course_frame,text="Department",font=("times new roman",13,"bold"),bg="white")
+        dep_label.grid(row=0,column=0,padx=10,sticky=W)
+
+        dep_combo=ttk.Combobox(current_course_frame,font=("times new roman",12,"bold"),width=17,state="read only")
+        dep_combo["values"]= ("Select Department", "Computer","IT","Civil","Mechanical")
+        dep_combo.current(0)
+        dep_combo.grid(row=0,column=1,padx=2,pady=10)
+
+        #Course
+        course_label=Label(current_course_frame,text="Course",font=("times new roman",13,"bold"),bg ="white")
+        course_label.grid(row=0,column=2,padx=10,sticky=W)
+
+        course_combo=ttk.Combobox(current_course_frame,font=("times new roman",13,"bold"),state="readonly",width=20)
+        course_combo["values"]=("Select Course","FE","SE","TE","BE")
+        course_combo.current(0)
+        course_combo.grid(row=0,column=3,padx=2,pady=10,sticky=W)
+        
+        #Year
+        year_label=Label(current_course_frame,text="Year",font=("times new roman",13,"bold"),bg="white")
+        year_label.grid(row=1,column=0,padx=10,sticky=W)
+
+        year_combo=ttk.Combobox(current_course_frame,font=("times new roman",13,"bold"),state="readonly",width=20)
+        year_combo["values"]=("select Year","2020-21","2021-22","2022-23","2023-24")
+        year_combo.current(0)
+        year_combo.grid(row=1,column=1,padx=2,pady=10,sticky=W)
+        
+        #Semester 
+        semester_label=Label(current_course_frame,text="Semester",font=("times new roman",13,"bold"),bg ="white")
+        semester_label.grid(row=1,column=2,padx=10,sticky=W)
+
+        semester_combo=ttk.Combobox(current_course_frame,font=("times new roman",13,"bold"),state="readonly",width=20)
+        semester_combo["values"]=("Select Semester","Semester-1","Semester-2")
+        semester_combo.current(0)
+        semester_combo.grid(row=1,column=3,padx=2,pady=10,sticky=W)
+
+        # Class Student information
+        class_Student_frame=LabelFrame(Left_frame,bd=2,bg="white",relief=RIDGE,text="Class Student information",font=("times new roman",12,"bold"))
+        class_Student_frame.place(x=5,y=250,width=720,height=300) 
+
+        studentId_label=Label(class_Student_frame,text="StudentID:",font=("times new roman",13,"bold"),bg ="white")
+        studentId_label.grid(row=0,column=0,padx=10,sticky=W)
+
+        studentID_entry=ttk.Entry(class_Student_frame,width=20,font=("times new roman",13,"bold"))
+        studentID_entry.grid(row=0,column=1,padx=10,sticky=W)
+        
+        
+       
+
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
